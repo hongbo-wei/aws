6ae8a7bd6bcc0be1eb6ddb33192603c7bd2cde9e
Hongbo Wei <hxw380@student.bham.ac.uk>
2024-10-05 00:00:53 +0400

    Update main.js

--- Bedrock/main.js
+++ Bedrock/main.js
@@ -4,6 +4,7 @@ import { BedrockRuntimeClient, ConverseCommand } from "@aws-sdk/client-bedrock-r
 // const modelId = 'anthropic.claude-3-haiku-20240307-v1:0';
 const modelId = 'anthropic.claude-v2';
 const prompt = "Give me an affirmation to boost my motivation today, referencing plants, animals, or flowers by adding emoji. Don't show the prompt, only the quote. Do not add anything like Here is an affirmation... just return the affirmation alone";
+// const prompt = ["Good Job", "Well Done!", "Distinction"]
 const conversation = [
   {
     role: "user",
